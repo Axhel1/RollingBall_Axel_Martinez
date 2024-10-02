34e6886a552318bcc0d87d65833b7fd435adaa63
Axhel1 <axelcomoaxelm888@gmail.com>
2024-10-02 18:11:55 +0200

    Texturas

--- Assets/Vectores.cs
+++ Assets/Vectores.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Vectores : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
