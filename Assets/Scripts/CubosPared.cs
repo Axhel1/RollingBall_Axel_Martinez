ae3ca75c481f222c12bd0e9b3ed6f21f35f78618
Axhel1 <axelcomoaxelm888@gmail.com>
2024-10-16 18:06:05 +0200

    camara lenta

--- Assets/Scripts/CubosPared.cs
+++ Assets/Scripts/CubosPared.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class CubosPared : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool inciartimer;
+
+
 
     // Update is called once per frame
     void Update()
     {
+        if (inciartimer) { 
+            
+            inciartimer = false;    
         
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) { 
+        
+            Time.timeScale = 0.2f;
+            inciartimer = true;
+
+
+        }
     }
 }
