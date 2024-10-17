7d0de2b46fee4834a864d08b47656143800aa162
Axhel1 <axelcomoaxelm888@gmail.com>
2024-10-17 19:15:12 +0200

    MovimientoConCamara

--- Assets/Scripts/Vectores.cs
+++ Assets/Scripts/Vectores.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class Vectores : MonoBehaviour
 {
     [SerializeField] float horizontalMove;
     [SerializeField] float verticalMove;
-    [SerializeField] float TargetAngulo;
-    [SerializeField] float Angulo;
+    [SerializeField] float targetAngulo;
+    [SerializeField] float angulo;
+    public float smoothTime = 0.1f;
+    float smoothVelocity;
     Vector3 moverPlayer;
+    Vector3 moverCam;
     CharacterController player;
+
+    public Transform Camara;
 
 
     [SerializeField] float playerSpeed;
@@ -36,18 +43,32 @@ public class Vectores : MonoBehaviour
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
 
-        moverPlayer = new Vector3(horizontalMove, 0, verticalMove).normalized; 
-        player.Move(moverPlayer * playerSpeed * Time.deltaTime);
+        moverPlayer = new Vector3(horizontalMove, 0, verticalMove).normalized;
 
-        TargetAngulo= Mathf.Atan2(moverPlayer.x,moverPlayer.z)*Mathf.Rad2Deg;
-        
+        if (moverPlayer.magnitude >= 0.1f) {
+
+            targetAngulo = Mathf.Atan2(moverPlayer.x, moverPlayer.z) * Mathf.Rad2Deg + Camara.eulerAngles.y;
+            angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngulo, ref smoothVelocity, smoothTime);
+            moverCam = Quaternion.Euler(0f, targetAngulo, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angulo, 0f);
+            player.Move(moverCam.normalized * playerSpeed * Time.deltaTime);
 
 
-        
-        
-        
-        
-        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         DetectarSuelo();
         /* if (Input.GetButtonDown("Jump")) {
 
