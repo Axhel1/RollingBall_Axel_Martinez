4308388a655ad0ba1141c0777f9a2e4e5976bba2
Axhel1 <axelcomoaxelm888@gmail.com>
2024-10-23 17:31:46 +0200

    movPlataformas

--- Assets/Scripts/PlataformasMove.cs
+++ Assets/Scripts/PlataformasMove.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlataformasMove : MonoBehaviour
+
+   
 {
-    // Start is called before the first frame update
+
+    [SerializeField]float speed;    
+    [SerializeField]float timer=0;
+
+
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        timer += 1 * Time.deltaTime;
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (timer >= 5) {
+
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+
+        }
+        
+
+
         
     }
 }
